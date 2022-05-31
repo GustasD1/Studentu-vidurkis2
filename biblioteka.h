f79ba5d8402b02da978dba97853cf5d2c8f4440e
GustasD1 <92514402+GustasD1@users.noreply.github.com>
2022-05-31 20:09:10 +0300

    Add files via upload

--- biblioteka.h
+++ biblioteka.h
@@ -37,6 +37,7 @@ using std::deque;
 
 class data{
 public:
+	//studentu klase
 	string vardas = "", pavarde = "";
 	vector<int>paz;
 	int egz = 0;
@@ -44,7 +45,31 @@ public:
 	double mediana = 0;
 	double rezultatasm = 0;
 	double vidurkis = 0;
+	//konstruktorius
+	data() {
+		vardas="";
+		pavarde="";
+		egz=0;
+		rezultatas=0;
+		mediana=0;
+		rezultatasm=0;
+		vidurkis=0;
+
+}
+	//destruktorius
 	~data();
+	//kopijavimo konstruktorius
+	data(const data& dataa) {
+		vardas = dataa.vardas;
+		pavarde = dataa.pavarde;
+		paz = dataa.paz;
+		egz = dataa.egz;
+		rezultatas = dataa.rezultatas;
+		mediana = dataa.mediana;
+		rezultatasm = dataa.rezultatasm;
+		vidurkis = dataa.vidurkis;
+	}
+	//Priskirimo konstruktorius
 	data& operator=(const data& dataa) {
 		if (&dataa == this) return *this;
 		vardas = dataa.vardas;
@@ -58,6 +83,16 @@ public:
 		return *this;
 
 	}
+	/*
+	* Ivedimo funkcija
+	*/
+	void ivestis(data& temp, int& n);
+	void isvedimas(const data& temp);
+	void apskaiciavimas(data& temp, int n);
+	void skaitymas(vector<data>& temp);
+	void FailoIsvedimas(vector<data>& temp, vector<data>& temp2);
+	void generacija(vector<data>& temp);
+	void skirstymas(vector<data>& temp, vector<data>& temp2);
 };
 
 
